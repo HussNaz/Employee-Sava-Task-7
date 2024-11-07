b3e8261332fa9ce50a04249ab8fb316543c281d9
HussNaz <nazmulhassanc09@gmail.com>
2024-11-07 13:13:45 +0600

    Commit for task-7

---
+++ src/main/java/com/example/demo/HelloController.java
@@ -0,0 +1,22 @@
+package com.example.demo;
+
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/hello")
+class HelloController {
+    @GetMapping
+    public String sayHello(@RequestParam(value = "name", defaultValue = "World") String name) {
+        return "Hello, " + name + "!";
+    }
+
+    @PostMapping
+    public String greetWithPost(@RequestBody NameRequest nameRequest) {
+        return "Hello, " + nameRequest.getName() + "!";
+    }
+
+    @PutMapping
+    public String greetWithPut(@RequestBody NameRequest nameRequest) {
+        return "Hello, " + nameRequest.getName() + " "+ nameRequest.getLastName()+" and email is "+ nameRequest.getEmail()+"!";
+    }
+}
